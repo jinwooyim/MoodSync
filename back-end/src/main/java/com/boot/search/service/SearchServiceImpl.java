5dbc47c53800fa43e4c094973025f9a7c025471f
임진우 <101246627+jinwooyim@users.noreply.github.com>
2025-06-02 12:37:45 +0900

    feat : 오타 검수 초안

--- back-end/src/main/java/com/boot/search/service/SearchServiceImpl.java
+++ back-end/src/main/java/com/boot/search/service/SearchServiceImpl.java
@@ -2,4 +2,16 @@ package com.boot.search.service;
 
 public class SearchServiceImpl implements SearchService {
 
+	private KeyboardMapper keyboardMapper;
+	private TypoCheck typoCheck;
+
+	@Override
+	public String resultText(String text) {
+
+		String Kor_text = keyboardMapper.convertEngToKor(text);
+		String result_text = typoCheck.combine(Kor_text);
+
+		return result_text;
+	}
+
 }

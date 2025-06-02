21b7dff84f7f86a63d468f0f96a7fca981b15ce9
임진우 <101246627+jinwooyim@users.noreply.github.com>
2025-06-02 11:34:44 +0900

    tes

--- back-end/src/main/java/com/boot/user/controller/UserController.java
+++ back-end/src/main/java/com/boot/user/controller/UserController.java
@@ -1,31 +1,18 @@
 package com.boot.user.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import com.boot.user.dto.BasicUserDTO;
-import com.boot.user.dto.UserDTO;
+
 import com.boot.user.service.UserService;
-import com.boot.z_config.security.UserUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -52,6 +39,7 @@ public class UserController {
 			}
 		}
 		return ResponseEntity.status(HttpStatus.CONFLICT).body("duplicate");
+
 	}
 
 	@RequestMapping("/user_info")

a84ff2f6fdf3f50e3529e96b7e2b4167da1a6822
Wjyuy <zhzhwm@naver.com>
2025-06-02 14:08:32 +0900

    test

--- back-end/src/main/java/com/boot/search/service/SearchServiceImpl.java
+++ back-end/src/main/java/com/boot/search/service/SearchServiceImpl.java
@@ -2,4 +2,10 @@ package com.boot.search.service;
 
 public class SearchServiceImpl implements SearchService {
 
+	@Override
+	public String resultText(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }

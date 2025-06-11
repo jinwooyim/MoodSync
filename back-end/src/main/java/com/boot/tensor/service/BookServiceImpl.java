6d27d5e663d427931c4d4a86d287e6a759a11475
DESKTOP-C85M9PN\user <nskynet5374@naver.com>
2025-06-11 11:13:46 +0900

    사용자감정데이터 부트로 전달

--- back-end/src/main/java/com/boot/tensor/service/BookServiceImpl.java
+++ back-end/src/main/java/com/boot/tensor/service/BookServiceImpl.java
@@ -28,24 +28,7 @@ public class BookServiceImpl implements BookService {
 	@Override
 	public ArrayList<BookDTO> getRandomBook(int emotionNumber) {
 		BookDAO dao = session.getMapper(BookDAO.class);
-		int[] random_num_array = new int[3];
 
-		Set<Integer> generated = new HashSet<>();
-
-		for (int i = 0; i < random_num_array.length;) {
-			int random_num = ((int) (Math.random() * 100) + (emotionNumber - 1) * 100 + 1);
-			if (!generated.contains(random_num)) {
-				generated.add(random_num);
-				random_num_array[i] = random_num;
-				i++; // i는 중복이 아닐 때만 증가
-			}
-		}
-
-//		int num_one = random_num_array[0];
-//		int num_two = random_num_array[1];
-//		int num_three = random_num_array[2];
-
-//		ArrayList<BookDTO> dtos = dao.getRandomBook(emotionNumber, num_one, num_two, num_three);
 		ArrayList<BookDTO> dtos = dao.getRandomBook(emotionNumber);
 
 		return dtos;

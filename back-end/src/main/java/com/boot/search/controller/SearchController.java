fc8e5112718795625e52d72339da99bd3b24472d
임진우 <101246627+jinwooyim@users.noreply.github.com>
2025-06-11 09:17:01 +0900

    엘라스틱 작업중 1

--- back-end/src/main/java/com/boot/search/controller/SearchController.java
+++ back-end/src/main/java/com/boot/search/controller/SearchController.java
@@ -1,13 +1,4 @@
 package com.boot.search.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@RestController
-@RequiredArgsConstructor
 public class SearchController {
-
 }

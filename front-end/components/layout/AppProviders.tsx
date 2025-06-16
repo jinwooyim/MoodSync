56e2bba4fbff30bfd6a39ae8062e218a04125fa4
임진우 <101246627+jinwooyim@users.noreply.github.com>
2025-06-16 14:30:17 +0900

    병합 충돌 수정

--- front-end/components/layout/AppProviders.tsx
+++ front-end/components/layout/AppProviders.tsx
@@ -34,16 +34,10 @@ export default function AppProviders({ children }: { children: React.ReactNode }
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <div className="flex flex-col min-h-screen "> 
         <Header /> 
-<<<<<<< HEAD
-        <main className="flex-1 bg-gradient-to-br from-purple-50 via-blue-50 to-pink-50 dark:bg-gray-900 text-gray-700">
-{loading ? ( // ⭐ 로딩 중일 때만 스피너와 메시지를 main 영역에 표시 ⭐
-            <div className="flex flex-col items-center justify-center h-full w-full"> {/* 스피너 컨테이너, main 영역 전체를 채우도록 */}
-=======
         {/* <main className="flex-1 bg-gradient-to-br from-purple-50 via-blue-50 to-pink-50"> */}
         <main className="min-h-screen bg-gradient-to-br from-purple-50 via-blue-50 to-pink-50 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 relative overflow-hidden transition-colors duration-300">
 {loading ? ( 
             <div className="flex flex-col items-center justify-center h-full w-full">
->>>>>>> 537d099c64e85108cc9f934df4fd4a7cfb2ec696
               <Spinner />
               <p className="mt-4 text-lg">인증 상태를 확인 중입니다...</p>
             </div>
